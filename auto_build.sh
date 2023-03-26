5e9e435a3c2cb4cdc368628f2617b738ec684741
川建国同志的奇妙加农炮之旅 <55585958+HYBB-rash@users.noreply.github.com>
2023-03-27 03:07:19 +0800

    1. 增加自动更新文章的功能 2. 排除 log 文件

--- auto_build.sh
+++ auto_build.sh
@@ -1,7 +1,32 @@
 #!/bin/bash
 
-# 从 GitHub 拉取代码更新
-git pull github master
+ROOT="/Users/hongyong/Projects/Blog"
+LOG_FILE="$ROOT/logs/update.log"
 
-# 执行 hexo generate
-hexo generate
+# 将当前时间戳写入日志文件
+echo "[$(date)] Checking for updates..." >> $LOG_FILE
+
+cd "$ROOT"
+
+remote_hash=$(git ls-remote https://github.com/HYBB-rash/Blog.git -h refs/heads/master | awk '{print $1}')
+local_hash=$(git rev-parse HEAD)
+
+echo "[$(date)] Remote hash $remote_hash. Local hash $local_hash." >> $LOG_FILE
+
+if [ "$remote_hash" != "$local_hash" ]; then
+
+    echo "[$(date)] Update found. Pulling changes and generating..." >> $LOG_FILE
+
+    # 从 GitHub 拉取代码更新
+    git pull github master >> $LOG_FILE 2>&1
+
+    # 执行 hexo generate
+    hexo generate >> $LOG_FILE 2>&1
+
+    echo "[$(date)] Update and generation completed." >> $LOG_FILE
+else
+    echo "[$(date)] No updates found." >> $LOG_FILE
+fi
+
+echo "[$(date)] .........................................................................................................." >> $LOG_FILE
+
