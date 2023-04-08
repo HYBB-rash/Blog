6c87beb0356702d5f7b21f375dd286202d42ab9f
川建国同志的奇妙加农炮之旅 <55585958+HYBB-rash@users.noreply.github.com>
2023-04-09 01:34:39 +0800

    Update site

--- public/sw.js
+++ public/sw.js
@@ -24,19 +24,19 @@ var precacheUrls = [];
 
   precacheUrls.push('/');
 
-  precacheUrls.push('/2023/03/27/texgt/');
-
   precacheUrls.push('/2023/04/09/hello-world/');
-
-  precacheUrls.push('/2023/03/27/test/');
-
-  precacheUrls.push('/2023/03/28/博客网站搭建总结/');
-
-  precacheUrls.push('/2023/03/27/自动构建脚本2-0/');
 
   precacheUrls.push('/2023/03/29/macOS的虚拟桌面和窗口管理/');
 
+  precacheUrls.push('/2023/03/28/博客网站搭建总结/');
+
   precacheUrls.push('/2023/03/27/自动构建脚本/');
+
+  precacheUrls.push('/2023/03/27/自动构建脚本2-0/');
+
+  precacheUrls.push('/2023/03/27/test/');
+
+  precacheUrls.push('/2023/03/27/texgt/');
 
 toolbox.precache(precacheUrls);
 toolbox.options = {"networkTimeoutSeconds":30};
