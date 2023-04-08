deea9accef511c30f04b4d4966d350bfd7880e32
川建国同志的奇妙加农炮之旅 <55585958+HYBB-rash@users.noreply.github.com>
2023-04-09 01:32:23 +0800

    Update site

--- auto_build.sh
+++ auto_build.sh
@@ -6,15 +6,13 @@ hexo generate
 # 2. 提交当前分支的所有修改
 git add .
 git commit -m "Update site"
+git push github master
 
 # 3. 切换到 build 分支并将 public 文件夹复制到该分支
 git checkout build
 git checkout master -- public/
 git add .
 git commit -m "Update build branch"
-
-# 4. 推送到远程仓库
-git push github master
 git push github build
 
 git checkout master
